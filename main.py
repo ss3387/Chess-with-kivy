c3b0998552e1dc098c3e6d872e1a02ebfb114a5c
Sridhar <62298338+12-sridhar@users.noreply.github.com>
2022-05-11 20:35:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from kivy.app import App
-from chess_game import main_game
+from chess_game import initiate_gui
 
 
 class ChessApp(App):
     def build(self):
         
-        self.game = main_game()
+        self.game = initiate_gui()
 
-        return self.game.gui
+        return self.game
+
 
 app = ChessApp()
 app.run()
