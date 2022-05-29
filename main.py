1fa618cbe90f5ac1a0d11e06871e734c114a82c1
Sridhar <62298338+12-sridhar@users.noreply.github.com>
2022-05-29 12:09:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from kivy.config import Config
 Config.set('graphics', 'window_state', 'maximized')
-#Config.set('graphics', 'resizable', False)
+Config.set('kivy', 'exit_on_escape', '0')
 
 from kivy.app import App
 from chess_game import initiate_gui
+from kivy.core.window import Window
 
 class ChessApp(App):
     def build(self):
         
         self.game = initiate_gui()
 
+        Window.bind(on_request_close=self.on_close)
+
         return self.game
+    
+    def on_close(self, *args):
+        if self.game.game_type == 'Online':
+            self.game.client.resign(True)
+        self.stop()
  
 app = ChessApp()
 app.run()
